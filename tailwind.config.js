2a3dceef02b5363963fb4e965523de3a0f3ab496
Gopikrishnan.R <33555597+gkr09@users.noreply.github.com>
2021-03-31 15:05:32 +0530

    Initial Upload
    tailwind.css is purged

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: ['./zaturn.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+colors:{
+yellow:{
+zaturn:'#FED32B'}},
+    fontFamily:{
+'sans':['Poppins']},
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
